baacdcd1d1bd861f50cd6233215095dfb0814c4b
xavialfaro <nxb.developer@gmail.com>
2022-06-01 23:39:50 -0500

    fix environment variables

--- src/constants/index.js
+++ src/constants/index.js
@@ -5,7 +5,7 @@ export const FETCH_OPTIONS = {
     'X-RapidAPI-Key': import.meta.env.VITE_RAPID_KEY
   }
 }
-console.log(FETCH_OPTIONS)
+console.log(process.env.VITE_RAPID_KEY)
 export const RAPIDAPI_HOST = 'https://weatherapi-com.p.rapidapi.com/'
 export const DEFAULT_QUERY = 'auto:ip'
 export const ENDPOINT_API = '/api/weather'
