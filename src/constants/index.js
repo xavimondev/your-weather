60aaea0a8a4b174253529daa813f88a2bcf3f6d9
xavialfaro <nxb.developer@gmail.com>
2022-06-02 00:15:29 -0500

    fix env variables netlify

--- src/constants/index.js
+++ src/constants/index.js
@@ -5,19 +5,6 @@ export const FETCH_OPTIONS = {
     'X-RapidAPI-Key': import.meta.env.VITE_RAPID_KEY
   }
 }
-const RAPID_KEY =
-  process.env.NODE_ENV === 'development'
-    ? import.meta.env.VITE_RAPID_KEY
-    : process.env.VITE_RAPID_KEY
-const RAPID_KEY_2 =
-  import.meta.env.MODE === 'development'
-    ? import.meta.env.VITE_RAPID_KEY
-    : process.env.VITE_RAPID_KEY
-
-console.log(`${RAPID_KEY} width process.env.NODE_ENV`)
-console.log(`${RAPID_KEY_2} width import.meta.env.VITE_RAPID_KEY`)
-console.log(import.meta.env)
-console.log(process.env)
 
 export const RAPIDAPI_HOST = 'https://weatherapi-com.p.rapidapi.com/'
 export const DEFAULT_QUERY = 'auto:ip'
